c465663496c7e49639aca82504827925c4013ce9
Vinod Kumar N <vinodkumar.nelanakula@carrier.com>
2024-04-03 09:41:28 +0530

    basic react with out JSX

---
+++ App.js
@@ -0,0 +1,33 @@
+const heading = React.createElement('h1', { id: 'heading' }, 'Hello world from React')
+const root = ReactDOM.createRoot(document.getElementById('root'))
+
+console.log('heading', heading);
+
+// Render neted elements in react
+const parent1 = React.createElement(
+  'div',
+  { id: 'parent' },
+  React.createElement(
+    'div',
+    { id: 'child' },
+    React.createElement(
+      'h1',
+      { id: 'heading' },
+      'Hello world from React in child')
+  )
+)
+
+const parent = React.createElement(
+  'div',
+  { id: 'parent' },
+  React.createElement(
+    'div',
+    { id: 'child' },
+   [ React.createElement('h1',{ id: 'heading' },'React child heading 1'),
+    React.createElement('h2', { id: 'heading2' },' React child heading 2'),
+    ]
+  )
+)
+console.log('parent', parent);
+root.render(heading)
+root.render(parent)

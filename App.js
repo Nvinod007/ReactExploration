e49bbc14a32e344c1f3e3fbdf842afb041c12b9d
Vinod Kumar N <vinodkumar.nelanakula@carrier.com>
2024-04-06 09:11:19 +0530

    playing with the elements and components

--- App.js
+++ App.js
@@ -3,17 +3,30 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
+const element = <span>I am an element </span>
 const Title = ()=>{
   return <h1>I am title</h1>
 }
+const TitleElement = (<h1>{element}I am title</h1>)
+const value = 99
+
 /**
  * 
  * @returns Browser will never know about components EOD it is a piece of HTML
+ * ? Component composition wraping component into another component
+ * * Component inside an Elemnt ==> valid
+ * * Element inside a Component ==> valid
+ * * Element inside an Elemnt ==> valid
+ * * Component inside a Component ==> valid
+ * 
  */
 const HeadComponent = ()=>{
   return (
     <div>
       <Title/>
+      {/* IN JSX WE CAN WRITE JS BY USING '{}' */}
+      {TitleElement}
+      {TitleElement + value}
       <h1>Header in functional component!</h1>
     </div>
     )

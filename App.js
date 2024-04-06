1ecf9c53d5985300f8ccd3af1772c977d86430b5
Vinod Kumar N <vinodkumar.nelanakula@carrier.com>
2024-04-06 08:52:08 +0530

    completely using JSX and components. priorly look over learn about JS and diff btw component and element branches

--- App.js
+++ App.js
@@ -1,36 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement('h1', { id: 'heading' }, 'Hello world from React')
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-console.log('heading', heading);
+const Title = ()=>{
+  return <h1>I am title</h1>
+}
+/**
+ * 
+ * @returns Browser will never know about components EOD it is a piece of HTML
+ */
+const HeadComponent = ()=>{
+  return (
+    <div>
+      <Title/>
+      <h1>Header in functional component!</h1>
+    </div>
+    )
+}
 
-// Render neted elements in react
-const parent1 = React.createElement(
-  'div',
-  { id: 'parent' },
-  React.createElement(
-    'div',
-    { id: 'child' },
-    React.createElement(
-      'h1',
-      { id: 'heading' },
-      'Hello world from React in child')
-  )
-)
-
-const parent = React.createElement(
-  'div',
-  { id: 'parent' },
-  React.createElement(
-    'div',
-    { id: 'child' },
-   [ React.createElement('h1',{ id: 'heading' },'React child heading 1'),
-    React.createElement('h2', { id: 'heading2' },' React child heading 2'),
-    ]
-  )
-)
-console.log('parent', parent);
-root.render(heading)
-root.render(parent)
+root.render(<HeadComponent />)

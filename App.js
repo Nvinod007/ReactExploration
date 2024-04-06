0a11559860bb787eaaad990659986ef8f7b55d05
Vinod Kumar N <vinodkumar.nelanakula@carrier.com>
2024-04-06 08:18:34 +0530

    Differences between JSX, HTML. How babel works.

--- App.js
+++ App.js
@@ -1,25 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-const heading = React.createElement('h1', { id: 'heading' }, 'Hello world from React')
 const root = ReactDOM.createRoot(document.getElementById('root'))
-
-console.log('heading', heading);
-
-// Render neted elements in react
-const parent1 = React.createElement(
-  'div',
-  { id: 'parent' },
-  React.createElement(
-    'div',
-    { id: 'child' },
-    React.createElement(
-      'h1',
-      { id: 'heading' },
-      'Hello world from React in child')
-  )
-)
-
 const parent = React.createElement(
   'div',
   { id: 'parent' },
@@ -31,6 +12,21 @@ const parent = React.createElement(
     ]
   )
 )
+
+/*
+* JSX - HTML/XML like syntax but not HTML or XML! JSX != HTML
+* JSX is not a valid JavaScript
+* JSX ==> React.CreateElement ==> React element - JS Object ==> HTML Element
+* Babel is converting the JSX to React.CreateElement
+* Babel is JS compiler, converts new latest code to old code for old browsers.
+* in HTML i have to 'Class' in JSX i have to write 'ClassName'.
+* But if you see renderd page -> inspect -> element you will class ultimately
+* Because JSX will be converted to HTML at the EOD
+*/
+const JSXParent = <div id='JSXParent'>
+  <h1 id='head1'>JSX child head1</h1>
+  <h1 id='head2' className='head1'>JSX child head2</h1>
+</div>
 console.log('parent', parent);
-root.render(heading)
-root.render(parent)
+console.log('parent', JSXParent);
+root.render(JSXParent)
